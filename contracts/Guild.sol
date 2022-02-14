93d0db5aba21b41282497654e0b132fbeda44708
Bruno Zell <brunoz.eth>
2022-02-13 17:43:53 -0700

    make bounties publushable

--- contracts/Guild.sol
+++ contracts/Guild.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "./OnboardingBounty.sol";
 
 contract Guild {
     // Todo: Implement IERC721 or create separate role NFT contract
 
     string private name;
-    address[] private members; 
+    address[] private members;
+    OnboardingBounty[] private activeBounties;
 
     constructor(string memory _name, address[] memory _initialMembers) {
         console.log("Creating new guild ", _name);
@@ -16,5 +18,10 @@ contract Guild {
 
     function getName() public view returns (string memory) {
         return name;
+    }
+
+    function publishBounty(string memory _ipfsDescription) public {
+        // Todo: Add deadline (?)
+        activeBounties.push(new OnboardingBounty(_ipfsDescription));
     }
 }
