968621a42fea9c68684db03b014ff48035f8cb9b
Bruno Zell <brunoz.eth>
2022-02-16 12:43:40 -0800

    add organization

---
+++ onchain/contracts/Organization.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "./OnboardingBounty.sol";
+import "./Guild.sol";
+
+contract Organization {
+    uint256 public memberCount;
+    mapping (address => bool) public members;
+
+    constructor(string memory name) {
+        members[msg.sender] = true;
+        memberCount = 1;
+    }
+
+    function leave() public {
+        members[account] = false;
+        memberCount -= 1;
+
+        // Todo: implement graceful organization shutdown in case no members are left
+    }
+
+    function isMember(address account) public view returns (bool) {
+        return members[account];
+    }
+
+    modifier onlyMember {
+        require(isMember(msg.sender));
+    }
+}
