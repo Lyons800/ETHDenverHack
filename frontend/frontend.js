4de0b42c89f33d99d614151c7f48e954e84eea58
Bruno Zell <brunoz.eth>
2022-02-19 22:36:20 -0700

    connect wallet

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,3 +1,12 @@
-function sendTx() {
-    console.log("lol")
-}
+const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+var currentAccount = null;
+
+const ConnectWallet = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const account = accounts[0];
+    console.log(account);
+    
+    var accountTextBox = document.getElementById("wallet-address");
+    accountTextBox.innerText = account;
+    currentAccount = account;
+  }
