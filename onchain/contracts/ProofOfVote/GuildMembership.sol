a93f0a361507ce7ca7712b4b2d9535db4985d299
Bruno Zell <brunoz.eth>
2022-02-20 06:12:25 -0700

    finish guild membership nft

--- onchain/contracts/ProofOfVote/GuildMembership.sol
+++ onchain/contracts/ProofOfVote/GuildMembership.sol
@@ -6,18 +6,29 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Guild.sol";
 
 contract GuildMembership is ERC721 {
+    Guild public origin;
     using Counters for Counters.Counter;
     Counters.Counter private _membershipIds;
 
-    constructor(string memory guildName) ERC721(guildName, "GLD") {
+    constructor(Guild memory _origin) ERC721(origin.name(), "GLD") {
+        origin = _origin;
     }
 
     function awardMembership(address member) public returns (uint256) {
+        require(origin.isMember(msg.sender));
+
         _membershipIds.increment();
 
         uint256 newMembershipId = _membershipIds.current();
         _mint(member, newMembershipId);
 
         return newMembershipId;
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal virtual override
+    {
+        // No transfer allowed
+        revert;
     }
 }
