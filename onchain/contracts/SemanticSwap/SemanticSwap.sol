3bb363732e2f45ce08a4e8373c04f9015693f995
Bruno Zell <brunoz.eth>
2022-02-20 06:33:32 -0700

    categorize potential matches

--- onchain/contracts/SemanticSwap/SemanticSwap.sol
+++ onchain/contracts/SemanticSwap/SemanticSwap.sol
@@ -12,14 +12,21 @@ contract SemanticSwap {
 
     OrderBook public orderBook;
     MatchProposal[] public matchProposals;
+    mapping (address => MatchProposal[]) matchProposalParticipation;
 
     function proposeMatch(string calldata reason, MatchLink[] calldata matchLinks) public {
         MatchProposal prop = new MatchProposal(reason);
         
         for (uint256 i = 0; i < matchLinks.length; i++) {
+            matchProposalParticipation[matchLinks[i].producer.participant()].push(prop);
+            matchProposalParticipation[matchLinks[i].consumer.participant()].push(prop);
             prop.AddLink(matchLinks[i].producer, matchLinks[i].consumer);
         }
         
         matchProposals.push(prop);
+    }
+
+    function listMatchProposals(address user) public view returns(MatchProposal[] memory proposals) {
+        return matchProposalParticipation[user];
     }
 }
